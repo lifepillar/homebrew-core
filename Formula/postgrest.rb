cb92523c4587e25004c67bdda9f50be83bfe678a
Lifepillar <lifepillar@lifepillar.me>
2017-03-03 11:24:50 +0100

    postgrest 0.4.0.0

--- Formula/postgrest.rb
+++ Formula/postgrest.rb
@@ -6,10 +6,10 @@ class Postgrest < Formula
 
   desc "Serves a fully RESTful API from any existing PostgreSQL database"
   homepage "https://github.com/begriffs/postgrest"
-  url "https://github.com/begriffs/postgrest/archive/v0.3.2.0.tar.gz"
-  sha256 "1cedceb22f051d4d80a75e4ac7a875164e3ee15bd6f6edc68dfca7c9265a2481"
-  head "https://github.com/begriffs/postgrest.git"
+  url "https://github.com/begriffs/postgrest/archive/v0.4.0.0.tar.gz"
+  sha256 "d23aa9b9ed0272dfd2075c573a96ba95e28328617ba63bfc2792f8655a479cb9"
   revision 1
+  head "https://github.com/begriffs/postgrest.git"
 
   bottle do
     sha256 "e46e739256e3f753abe8540db32cff90ed8e4adfbed1e658cb229dc8ded0ce00" => :sierra
